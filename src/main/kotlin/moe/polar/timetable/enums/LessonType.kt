1ce30f81c7ea8d97367a6e68c629e64031550e3a
httpolar <96192210+httpolar@users.noreply.github.com>
2023-10-20 23:23:55 +0400

    LessonType should be serializable

--- src/main/kotlin/moe/polar/timetable/enums/LessonType.kt
+++ src/main/kotlin/moe/polar/timetable/enums/LessonType.kt
@@ -1,5 +1,8 @@
 package moe.polar.timetable.enums
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class LessonType(override val value: Int) : IntEnum {
     LECTURE(1),
     PRACTICUM(2),
